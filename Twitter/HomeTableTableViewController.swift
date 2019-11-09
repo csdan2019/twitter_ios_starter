03f7dfb70f7a0234158a8f87f42b50ccdf79eec1
Dan <tonthatd@gmail.com>
2019-11-08 22:47:49 -0800

    part 2 twitter

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -19,10 +19,15 @@ class HomeTableTableViewController: UITableViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+       
         
 
     
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         loadTweet()
     }
 
     func loadTweet(){
@@ -57,6 +62,12 @@ class HomeTableTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+    
         return cell
     }
     // MARK: - Table view data source
